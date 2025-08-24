d0d5ce367478a25a9873afaef71db779147766c0
Raaid-Shaheer <sahlraaid52@gmail.com>
2025-08-24 08:58:01 +0530

    Delete task function added

--- notesdemo.py
+++ notesdemo.py
@@ -12,14 +12,34 @@ except FileNotFoundError:
 
 #commands
 
+# check if command is given
+
+if len(sys.argv) <2:
+  print("Please follow the format: filename function note")
+  sys.exit(1)
+#add task
 if sys.argv[1]== "add":
   NewAddition = "".join(sys.argv[2:])
   notesdemo.append(NewAddition)
   with open(FILENAME,"w") as f:
     json.dump(notesdemo,f,indent=4)
     print("Note Added Successfully:", NewAddition)
-
+# list all tasks
 elif sys.argv[1] == "list":
   for i, listThis in enumerate(notesdemo,1):
     print(f"{i}.{listThis}")
-  
+
+# delete a task
+
+elif sys.argv[1] == "delete":
+  if len(sys.argv) <3:
+    print("Pleasee enter the note number you want to delete after the command delete")
+  else:
+    try:
+      note_index = int(sys.argv[2]) - 1 # to make this 0-index
+      deleted_note= notesdemo.pop(note_index)
+      with open(FILENAME,"w") as f:
+        json.dump(notesdemo,f,indent=4)
+        print(f"Deleted Note: {deleted_note}") 
+    except(ValueError,IndexError):
+      print("Invalid Note Number") 
